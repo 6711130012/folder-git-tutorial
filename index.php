dc435104952c29ce26da0656f018f06942af1353
JIN <6711130012@mut.ac.th>
2026-07-02 17:22:53 +0700

    commit 1

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php 
+     echo "wowowowow"
+     echo "whywhwhywhwy"
+?> 
---
+++ myname.php
@@ -0,0 +1,7 @@
+<?php
+     echo "Hello Git HUB";
+     zxczxcxza:''
+     echo "My name is Jin";
+     echo "my JIN"
+     echo "go away"
+?>  
